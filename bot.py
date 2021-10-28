0bbf43f87b60a1dd3e7cc201bb64a5d19d267898
chsurvey <58624692+chsurvey@users.noreply.github.com>
2021-10-28 19:03:35 +0900

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,167 @@
+# -*- coding: utf-8 -*-
+total = {'기상 41236lp': 'WS+hcf+1, ', '앉은자세 41236lp': 'FC+hcf+1, ', '뒤자세 41236lp': 'BT+hcf+1, ', '기상 41236rp': 'WS+hcf+2, ', '앉은자세 41236rp': 'FC+hcf+2, ', '뒤자세 41236rp': 'BT+hcf+2, ', '기상 41236lk': 'WS+hcf+3, ', '앉은자세 41236lk': 'FC+hcf+3, ', '뒤자세 41236lk': 'BT+hcf+3, ', '기상 41236rk': 'WS+hcf+4, ', '앉은자세 41236rk': 'FC+hcf+4, ', '뒤자세 41236rk': 'BT+hcf+4, ', '기상 41236ap': 'WS+hcf+1+2, ', '앉은자세 41236ap': 'FC+hcf+1+2, ', '뒤자세 41236ap': 'BT+hcf+1+2, ', '기상 41236ak': 'WS+hcf+3+4, ', '앉은자세 41236ak': 'FC+hcf+3+4, ', '뒤자세 41236ak': 'BT+hcf+3+4, ', '기상 63214lp': 'WS+hcb+1, ', '앉은자세 63214lp': 'FC+hcb+1, ', '뒤자세 63214lp': 'BT+hcb+1, ', '기상 63214rp': 'WS+hcb+2, ', '앉은자세 63214rp': 'FC+hcb+2, ', '뒤자세 63214rp': 'BT+hcb+2, ', '기상 63214lk': 'WS+hcb+3, ', '앉은자세 63214lk': 'FC+hcb+3, ', '뒤자세 63214lk': 'BT+hcb+3, ', '기상 63214rk': 'WS+hcb+4, ', '앉은자세 63214rk': 'FC+hcb+4, ', '뒤자세 63214rk': 'BT+hcb+4, ', '기상 63214ap': 'WS+hcb+1+2, ', '앉은자세 63214ap': 'FC+hcb+1+2, ', '뒤자세 63214ap': 'BT+hcb+1+2, ', '기상 63214ak': 'WS+hcb+3+4, ', '앉은자세 63214ak': 'FC+hcb+3+4, ', '뒤자세 63214ak': 'BT+hcb+3+4, ', '기상 236lp': 'WS+qcf+1, ', '앉은자세 236lp': 'FC+qcf+1, ', '뒤자세 236lp': 'BT+qcf+1, ', '기상 236rp': 'WS+qcf+2, ', '앉은자세 236rp': 'FC+qcf+2, ', '뒤자세 236rp': 'BT+qcf+2, ', '기상 236lk': 'WS+qcf+3, ', '앉은자세 236lk': 'FC+qcf+3, ', '뒤자세 236lk': 'BT+qcf+3, ', '기상 236rk': 'WS+qcf+4, ', '앉은자세 236rk': 'FC+qcf+4, ', '뒤자세 236rk': 'BT+qcf+4, ', '기상 236ap': 'WS+qcf+1+2, ', '앉은자세 236ap': 'FC+qcf+1+2, ', '뒤자세 236ap': 'BT+qcf+1+2, ', '기상 236ak': 'WS+qcf+3+4, ', '앉은자세 236ak': 'FC+qcf+3+4, ', '뒤자세 236ak': 'BT+qcf+3+4, ', '기상 214lp': 'WS+qcb+1, ', '앉은자세 214lp': 'FC+qcb+1, ', '뒤자세 214lp': 'BT+qcb+1, ', '기상 214rp': 'WS+qcb+2, ', '앉은자세 214rp': 'FC+qcb+2, ', '뒤자세 214rp': 'BT+qcb+2, ', '기상 214lk': 'WS+qcb+3, ', '앉은자세 214lk': 'FC+qcb+3, ', '뒤자세 214lk': 'BT+qcb+3, ', '기상 214rk': 'WS+qcb+4, ', '앉은자세 214rk': 'FC+qcb+4, ', '뒤자세 214rk': 'BT+qcb+4, ', '기상 214ap': 'WS+qcb+1+2, ', '앉은자세 214ap': 'FC+qcb+1+2, ', '뒤자세 214ap': 'BT+qcb+1+2, ', '기상 214ak': 'WS+qcb+3+4, ', '앉은자세 214ak': 'FC+qcb+3+4, ', '뒤자세 214ak': 'BT+qcb+3+4, ', '기상 1lp': 'WS+d/b+1, ', '앉은자세 1lp': 'FC+d/b+1, ', '뒤자세 1lp': 'BT+d/b+1, ', '기상 1rp': 'WS+d/b+2, ', '앉은자세 1rp': 'FC+d/b+2, ', '뒤자세 1rp': 'BT+d/b+2, ', '기상 1lk': 'WS+d/b+3, ', '앉은자세 1lk': 'FC+d/b+3, ', '뒤자세 1lk': 'BT+d/b+3, ', '기상 1rk': 'WS+d/b+4, ', '앉은자세 1rk': 'FC+d/b+4, ', '뒤자세 1rk': 'BT+d/b+4, ', '기상 1ap': 'WS+d/b+1+2, ', '앉은자세 1ap': 'FC+d/b+1+2, ', '뒤자세 1ap': 'BT+d/b+1+2, ', '기상 1ak': 'WS+d/b+3+4, ', '앉은자세 1ak': 'FC+d/b+3+4, ', '뒤자세 1ak': 'BT+d/b+3+4, ', '기상 2lp': 'WS+d+1, ', '앉은자세 2lp': 'FC+d+1, ', '뒤자세 2lp': 'BT+d+1, ', '기상 2rp': 'WS+d+2, ', '앉은자세 2rp': 'FC+d+2, ', '뒤자세 2rp': 'BT+d+2, ', '기상 2lk': 'WS+d+3, ', '앉은자세 2lk': 'FC+d+3, ', '뒤자세 2lk': 'BT+d+3, ', '기상 2rk': 'WS+d+4, ', '앉은자세 2rk': 'FC+d+4, ', '뒤자세 2rk': 'BT+d+4, ', '기상 2ap': 'WS+d+1+2, ', '앉은자세 2ap': 'FC+d+1+2, ', '뒤자세 2ap': 'BT+d+1+2, ', '기상 2ak': 'WS+d+3+4, ', '앉은자세 2ak': 'FC+d+3+4, ', '뒤자세 2ak': 'BT+d+3+4, ', '기상 3lp': 'WS+d/f+1, ', '앉은자세 3lp': 'FC+d/f+1, ', '뒤자세 3lp': 'BT+d/f+1, ', '기상 3rp': 'WS+d/f+2, ', '앉은자세 3rp': 'FC+d/f+2, ', '뒤자세 3rp': 'BT+d/f+2, ', '기상 3lk': 'WS+d/f+3, ', '앉은자세 3lk': 'FC+d/f+3, ', '뒤자세 3lk': 'BT+d/f+3, ', '기상 3rk': 'WS+d/f+4, ', '앉은자세 3rk': 'FC+d/f+4, ', '뒤자세 3rk': 'BT+d/f+4, ', '기상 3ap': 'WS+d/f+1+2, ', '앉은자세 3ap': 'FC+d/f+1+2, ', '뒤자세 3ap': 'BT+d/f+1+2, ', '기상 3ak': 'WS+d/f+3+4, ', '앉은자세 3ak': 'FC+d/f+3+4, ', '뒤자세 3ak': 'BT+d/f+3+4, ', '기상 4lp': 'WS+b+1, ', '앉은자세 4lp': 'FC+b+1, ', '뒤자세 4lp': 'BT+b+1, ', '기상 4rp': 'WS+b+2, ', '앉은자세 4rp': 'FC+b+2, ', '뒤자세 4rp': 'BT+b+2, ', '기상 4lk': 'WS+b+3, ', '앉은자세 4lk': 'FC+b+3, ', '뒤자세 4lk': 'BT+b+3, ', '기상 4rk': 'WS+b+4, ', '앉은자세 4rk': 'FC+b+4, ', '뒤자세 4rk': 'BT+b+4, ', '기상 4ap': 'WS+b+1+2, ', '앉은자세 4ap': 'FC+b+1+2, ', '뒤자세 4ap': 'BT+b+1+2, ', '기상 4ak': 'WS+b+3+4, ', '앉은자세 4ak': 'FC+b+3+4, ', '뒤자세 4ak': 'BT+b+3+4, ', '기상 Nlp': 'WS+n+1, ', '앉은자세 Nlp': 'FC+n+1, ', '뒤자세 Nlp': 'BT+n+1, ', '기상 Nrp': 'WS+n+2, ', '앉은자세 Nrp': 'FC+n+2, ', '뒤자세 Nrp': 'BT+n+2, ', '기상 Nlk': 'WS+n+3, ', '앉은자세 Nlk': 'FC+n+3, ', '뒤자세 Nlk': 'BT+n+3, ', '기상 Nrk': 'WS+n+4, ', '앉은자세 Nrk': 'FC+n+4, ', '뒤자세 Nrk': 'BT+n+4, ', '기상 Nap': 'WS+n+1+2, ', '앉은자세 Nap': 'FC+n+1+2, ', '뒤자세 Nap': 'BT+n+1+2, ', '기상 Nak': 'WS+n+3+4, ', '앉은자세 Nak': 'FC+n+3+4, ', '뒤자세 Nak': 'BT+n+3+4, ', '기상 6lp': 'WS+f+1, ', '앉은자세 6lp': 'FC+f+1, ', '뒤자세 6lp': 'BT+f+1, ', '기상 6rp': 'WS+f+2, ', '앉은자세 6rp': 'FC+f+2, ', '뒤자세 6rp': 'BT+f+2, ', '기상 6lk': 'WS+f+3, ', '앉은자세 6lk': 'FC+f+3, ', '뒤자세 6lk': 'BT+f+3, ', '기상 6rk': 'WS+f+4, ', '앉은자세 6rk': 'FC+f+4, ', '뒤자세 6rk': 'BT+f+4, ', '기상 6ap': 'WS+f+1+2, ', '앉은자세 6ap': 'FC+f+1+2, ', '뒤자세 6ap': 'BT+f+1+2, ', '기상 6ak': 'WS+f+3+4, ', '앉은자세 6ak': 'FC+f+3+4, ', '뒤자세 6ak': 'BT+f+3+4, ', '기상 7lp': 'WS+u/b+1, ', '앉은자세 7lp': 'FC+u/b+1, ', '뒤자세 7lp': 'BT+u/b+1, ', '기상 7rp': 'WS+u/b+2, ', '앉은자세 7rp': 'FC+u/b+2, ', '뒤자세 7rp': 'BT+u/b+2, ', '기상 7lk': 'WS+u/b+3, ', '앉은자세 7lk': 'FC+u/b+3, ', '뒤자세 7lk': 'BT+u/b+3, ', '기상 7rk': 'WS+u/b+4, ', '앉은자세 7rk': 'FC+u/b+4, ', '뒤자세 7rk': 'BT+u/b+4, ', '기상 7ap': 'WS+u/b+1+2, ', '앉은자세 7ap': 'FC+u/b+1+2, ', '뒤자세 7ap': 'BT+u/b+1+2, ', '기상 7ak': 'WS+u/b+3+4, ', '앉은자세 7ak': 'FC+u/b+3+4, ', '뒤자세 7ak': 'BT+u/b+3+4, ', '기상 8lp': 'WS+u+1, ', '앉은자세 8lp': 'FC+u+1, ', '뒤자세 8lp': 'BT+u+1, ', '기상 8rp': 'WS+u+2, ', '앉은자세 8rp': 'FC+u+2, ', '뒤자세 8rp': 'BT+u+2, ', '기상 8lk': 'WS+u+3, ', '앉은자세 8lk': 'FC+u+3, ', '뒤자세 8lk': 'BT+u+3, ', '기상 8rk': 'WS+u+4, ', '앉은자세 8rk': 'FC+u+4, ', '뒤자세 8rk': 'BT+u+4, ', '기상 8ap': 'WS+u+1+2, ', '앉은자세 8ap': 'FC+u+1+2, ', '뒤자세 8ap': 'BT+u+1+2, ', '기상 8ak': 'WS+u+3+4, ', '앉은자세 8ak': 'FC+u+3+4, ', '뒤자세 8ak': 'BT+u+3+4, ', '기상 9lp': 'WS+u/f+1, ', '앉은자세 9lp': 'FC+u/f+1, ', '뒤자세 9lp': 'BT+u/f+1, ', '기상 9rp': 'WS+u/f+2, ', '앉은자세 9rp': 'FC+u/f+2, ', '뒤자세 9rp': 'BT+u/f+2, ', '기상 9lk': 'WS+u/f+3, ', '앉은자세 9lk': 'FC+u/f+3, ', '뒤자세 9lk': 'BT+u/f+3, ', '기상 9rk': 'WS+u/f+4, ', '앉은자세 9rk': 'FC+u/f+4, ', '뒤자세 9rk': 'BT+u/f+4, ', '기상 9ap': 'WS+u/f+1+2, ', '앉은자세 9ap': 'FC+u/f+1+2, ', '뒤자세 9ap': 'BT+u/f+1+2, ', '기상 9ak': 'WS+u/f+3+4, ', '앉은자세 9ak': 'FC+u/f+3+4, ', '뒤자세 9ak': 'BT+u/f+3+4, ', '41236lp': 'hcf+1, ', '41236rp': 'hcf+2, ', '41236lk': 'hcf+3, ', '41236rk': 'hcf+4, ', '41236ap': 'hcf+1+2, ', '41236ak': 'hcf+3+4, ', '63214lp': 'hcb+1, ', '63214rp': 'hcb+2, ', '63214lk': 'hcb+3, ', '63214rk': 'hcb+4, ', '63214ap': 'hcb+1+2, ', '63214ak': 'hcb+3+4, ', '236lp': 'qcf+1, ', '236rp': 'qcf+2, ', '236lk': 'qcf+3, ', '236rk': 'qcf+4, ', '236ap': 'qcf+1+2, ', '236ak': 'qcf+3+4, ', '214lp': 'qcb+1, ', '214rp': 'qcb+2, ', '214lk': 'qcb+3, ', '214rk': 'qcb+4, ', '214ap': 'qcb+1+2, ', '214ak': 'qcb+3+4, ', '1lp': 'd/b+1, ', '1rp': 'd/b+2, ', '1lk': 'd/b+3, ', '1rk': 'd/b+4, ', '1ap': 'd/b+1+2, ', '1ak': 'd/b+3+4, ', '2lp': 'd+1, ', '2rp': 'd+2, ', '2lk': 'd+3, ', '2rk': 'd+4, ', '2ap': 'd+1+2, ', '2ak': 'd+3+4, ', '3lp': 'd/f+1, ', '3rp': 'd/f+2, ', '3lk': 'd/f+3, ', '3rk': 'd/f+4, ', '3ap': 'd/f+1+2, ', '3ak': 'd/f+3+4, ', '4lp': 'b+1, ', '4rp': 'b+2, ', '4lk': 'b+3, ', '4rk': 'b+4, ', '4ap': 'b+1+2, ', '4ak': 'b+3+4, ', 'Nlp': 'n+1, ', 'Nrp': 'n+2, ', 'Nlk': 'n+3, ', 'Nrk': 'n+4, ', 'Nap': 'n+1+2, ', 'Nak': 'n+3+4, ', '6lp': 'f+1, ', '6rp': 'f+2, ', '6lk': 'f+3, ', '6rk': 'f+4, ', '6ap': 'f+1+2, ', '6ak': 'f+3+4, ', '7lp': 'u/b+1, ', '7rp': 'u/b+2, ', '7lk': 'u/b+3, ', '7rk': 'u/b+4, ', '7ap': 'u/b+1+2, ', '7ak': 'u/b+3+4, ', '8lp': 'u+1, ', '8rp': 'u+2, ', '8lk': 'u+3, ', '8rk': 'u+4, ', '8ap': 'u+1+2, ', '8ak': 'u+3+4, ', '9lp': 'u/f+1, ', '9rp': 'u/f+2, ', '9lk': 'u/f+3, ', '9rk': 'u/f+4, ', '9ap': 'u/f+1+2, ', '9ak': 'u/f+3+4, ', '41236': 'hcf, ', '63214': 'hcb, ', '236': 'qcf, ', '214': 'qcb, ', '1': 'd/b, ', '2': 'd, ', '3': 'd/f, ', '4': 'b, ', 'N': 'n, ', '6': 'f, ', '7': 'u/b, ', '8': 'u, ', '9': 'u/f, ', 'lp': '1, ', 'rp': '2, ', 'lk': '3, ', 'rk': '4, ', 'ap': '1+2, ', 'ak': '3+4, '}
+keys = list(total.keys())
+
+def ko2en(ko):
+    l=len(ko)
+    for i in range(1,l):
+        if(ko[i].isdecimal() and not ko[i-1].isdecimal()):
+            ko.insert(i,",")
+    ans = ko.split(",")
+    ret = []
+    for part in ans:
+        for key in keys:
+            if part.find(key) != -1:
+                part=part.replace(key,total[key])
+        ret.append(part)
+    fin = ', '.join(ret)
+
+    return fin[:len(fin)-1]
+
+import discord
+import os
+import json
+
+character = ["Akuma", "Alisa", "Anna", "Armor King", "Asuka", "Bob", "Bryan", "Claudio", "Devil Jin", "Dragunov", "Eddy", "Eliza", "Fahkumram", "Feng", "Ganryu", "Geese", "Gigas", "Heihachi", "Hwoarang", "Jack-7", "Jin", "Josie", "Julia", "Katarina", "Kazumi", "Kazuya", "King","Kuma Panda", "Kunimitsu", "Lars", "Law", "Lee", "Lei", "Leo", "Leroy", "Lidia", "Lili", "Lucky Chloe", "Marduk", "Master Raven", "Miguel", "Negan", "Nina", "Noctis", "Paul", "Shaheen", "Steve", "Xiaoyu", "Yoshimitsu", "Zafina"]
+
+dic_num=903198427494641704
+
+client = discord.Client()
+guild = discord.Guild
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Game(name="-도움"))
+    print("Ready")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    elif message.content.startswith('-'):
+
+        cmd = message.content.split()[0].replace("-","")
+
+        if len(message.content.split()) > 1:
+            parameters = message.content.split()[1:]
+
+        # if cmd == 'dm':
+        #     user = await client.fetch_user(int(parameters[0]))
+        #     await user.create_dm()
+        #     channel = user.dm_channel
+        #     await channel.send("test")
+        if cmd == "프레임":
+            
+            print(parameters)
+
+            # char_dic채널의 dictionary 읽어들이기
+            ms_dic = ""
+            command = ""
+            dic_C = client.get_channel(dic_num)
+            async for msg in dic_C.history(limit=1):
+                ms_dic = msg.content
+            db_dic = json.loads(ms_dic)
+            
+            if parameters[0] in db_dic:
+                parameters[0] = db_dic[parameters[0]]
+                
+            parameters[0]=parameters[0].lower()
+            parameters[0]=parameters[0].capitalize()
+
+            if parameters[0] in character or parameters[0] == "Kuma" or parameters[0] == "Panda":
+                if(parameters[0] == "Kuma" or parameters[0] == "Panda"):
+                    parameters[0] == "Kuma Panda"
+
+                # 찾으려는 캐릭터 이름
+                name = parameters[0]
+                
+                # 해당 캐릭터의 json 파싱
+                with open('./json/'+name+'.json') as json_file:
+                    total_data = json.load(json_file)
+                
+                # dic채널의 dictionary 읽어들이기
+                db_dic = ''
+                dic_m = client.get_channel(dic_num)
+                async for msg in dic_m.history(limit=1):
+                    ms_dic = msg.content
+                db_dic = json.loads(ms_dic)
+                
+                command=""
+
+                if(parameters[1] in db_dic.keys()):
+                   command = db_dic[parameters[1]]    
+
+                else:
+                    # 입력받은 커맨드 영어로 전환
+                    command = ko2en(parameters[1].lower())
+                    print(command)
+
+                    # 커맨드를 일부 포함하고 있는 모든 기술 저장
+                    ans_candidate = []
+                    keys = list(total_data.keys())
+                    for db_command in keys:
+                        if command in db_command:
+                            ans_candidate.append(db_command)
+                    
+                    print(ans_candidate)
+                    # 커맨드를 일부 포함하고 있는 모든 기술 embed 형식으로 출력 & 커맨드 인덱싱
+                    embed=discord.Embed(title="다음 중 찾는 기술의 번호를 입력하세요", color=0x00d9ff)
+                    indexed_key = []
+                    for idx, val in enumerate(ans_candidate):
+                        embed.add_field(name = str(idx+1) + ". ", value=val, inline=True)
+                        indexed_key.append(val)
+                    await message.channel.send(embed=embed)
+
+                    # 유저의 번호 고르기 대기
+                    def check(msg):
+                        return msg.author == message.author and msg.channel == message.channel and msg.content.isdecimal()
+                        
+                    msg = await client.wait_for("message", check=check)
+                    
+                    # 최종적으로 정보를 찾을 커맨드 선택
+                    command = indexed_key[int(msg.content) - 1]
+
+                # total_data에서 정보를 구분자 |로 파싱
+                stat = total_data[command].split("|")
+
+                #판정|데미지|발동|가드시 프레임|히트시 프레임|카운터시 프레임|비고
+                print(stat)
+                embed=discord.Embed(title=command, color=0x00d9ff)
+                embed.add_field(name="판정", value=stat[0], inline=True)
+                embed.add_field(name="데미지", value=stat[1], inline=True)
+                embed.add_field(name="발동", value=stat[2], inline=True)
+                embed.add_field(name="가드시", value=stat[3], inline=True)
+                embed.add_field(name="히트시", value=stat[4], inline=True)
+                embed.add_field(name="카운터시", value=stat[5], inline=True)
+                await message.channel.send(embed=embed)
+
+                # TODO: 약어로 찾기 추가
+
+        if cmd == "약어추가":
+            name = ""
+            command=""
+            ms_dic=""
+            dic_C = client.get_channel(dic_num)
+            async for msg in dic_C.history(limit=1):
+                ms_dic = msg.content
+                await msg.delete()
+            if(len(ms_dic)>0):
+                db_dic = json.loads(ms_dic)
+
+            try:
+                exsisting = db_dic[parameters[1]]
+            except:
+                exsisting = "None"
+
+            try:
+                db_dic[parameters[1]] = parameters[0]
+            except:
+                db_dic = {}
+
+            print(db_dic)
+            await dic_C.send(json.dumps(db_dic, ensure_ascii = False))
+            await message.channel.send("약어 추가 완료 "+exsisting+" -> "+parameters[0])
+
+access_token = "OTAzMTg4ODk3NDY5NzI2ODAw.YXpWNQ.y4C1XaSNGpzZsHoytlRQ52hcI0E" # os.environ["BOT_TOKEN"]
+client.run(access_token)
